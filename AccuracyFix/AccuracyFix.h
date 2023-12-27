3557663f9e29c8e3798c2862b95c93a5118c280b
thinkk <50614694+think4k@users.noreply.github.com>
2023-12-28 00:42:51 +0100

    Update AccuracyFix.h

--- AccuracyFix/AccuracyFix.h
+++ AccuracyFix/AccuracyFix.h
@@ -20,9 +20,6 @@ public:
 	cvar_t* CvarRegister(const char* Name, const char* Value);
 
 private:
-#ifndef ACCURACY_DISABLE_RECOIL_CONTROL
-	std::map<int, P_PLAYER_DATA> m_Data;
-#endif
 	std::map<std::string, cvar_t> m_Cvar;
 
 	cvar_t* m_af_accuracy_all;
