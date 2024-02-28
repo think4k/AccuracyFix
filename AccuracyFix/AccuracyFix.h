0500bebd1a4ab187cddb874008f1d848ce49f577
thinkk <50614694+think4k@users.noreply.github.com>
2024-02-28 15:46:12 +0100

    Update AccuracyFix.h

--- AccuracyFix/AccuracyFix.h
+++ AccuracyFix/AccuracyFix.h
@@ -2,21 +2,22 @@
 
 class CAccuracyFix
 {
-public:
-	void ServerActivate();
 
-	void TraceLine(const float* vStart, const float* vEnd, int fNoMonsters, edict_t* pentToSkip, TraceResult* ptr);
-	float GetUserAiming(edict_t* edict, int* cpId, int* cpBody, float distance);
-	cvar_t* CvarRegister(const char* Name, const char* Value);
+	public:
+		void ServerActivate();
 
-private:
-	std::map<std::string, cvar_t> m_Cvar;
+		void TraceLine(const float* vStart, const float* vEnd, int fNoMonsters, edict_t* pentToSkip, TraceResult* ptr);
+		float GetUserAiming(edict_t* edict, int* cpId, int* cpBody, float distance);
+		cvar_t* CvarRegister(const char* Name, const char* Value);
 
-	cvar_t* m_af_accuracy_all;
-	std::array<cvar_t*, MAX_WEAPONS + 1> m_af_accuracy;
+	private:
+		std::map<std::string, cvar_t> m_Cvar;
 
-	cvar_t* m_af_distance_all;
-	std::array<cvar_t*, MAX_WEAPONS + 1> m_af_distance;
+		cvar_t* m_af_accuracy_all;
+		std::array<cvar_t*, MAX_WEAPONS + 1> m_af_accuracy;
+
+		cvar_t* m_af_distance_all;
+		std::array<cvar_t*, MAX_WEAPONS + 1> m_af_distance;
 };
 
 extern CAccuracyFix gAccuracyFix;
